66ca88be0e452020618ae6bb0ead3f5826087b28
churysungita <azizisungita5@gmail.com>
2022-05-11 00:00:04 +0300

    tenth commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,12 @@ userimage.addEventListener('click', function() {
 cross.addEventListener('click', function() {
     overlay.classList.add('d-none');
 });
+
+//  Get this script ready when the page loads
+$(document).ready(function() {
+    //   Create a function
+    $(".arrow").click(function(event) {
+        //       Select the body of the page and scroll down by 650 pixels worth
+        $("html, body").animate({ scrollTop: "+=650px" }, 800);
+    });
+});
